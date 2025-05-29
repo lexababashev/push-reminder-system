a3863ac88c8822d810302e3b6d37dd18b8c6ca2e
Alex <alex.babashev@gmail.com>
2025-05-30 02:55:17 +0300

    fix

--- apps/notification/src/main.ts
+++ apps/notification/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { NotificationModule } from './notification.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('NotificationApp');
   const app = await NestFactory.create(NotificationModule);
-  await app.listen(process.env.port ?? 3001);
+  const port = process.env.PORT_N ?? 3001;
+  await app.listen(port);
+  logger.log(`Notification service is running on: http://localhost:${port}`);
 }
 bootstrap();
--- apps/user/src/main.ts
+++ apps/user/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('UserApp');
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT_U ?? 3000;
+  await app.listen(port);
+  logger.log(`User service is running on: http://localhost:${port}/api`);
 }
 bootstrap();
